fa2c90cbbb91e74986acb9af71786fd3b60b7934
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-05-05 12:14:05 -0700

    style changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var today = new Date();
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'cctober', 'november', 'december'];
 document.querySelectorAll('#date').forEach(dateElement => {
   dateElement.innerHTML += ' ' + months[today.getMonth()] + ' ' + today.getDate() + ', ' + today.getFullYear();
+
 });
 
 const blue = document.querySelector('.blue');
