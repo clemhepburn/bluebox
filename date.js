b6972bae41fc55841797e51d530ef8197fcd2586
Clem Hepburn <cooper.hepburn@pcc.edu>
2022-02-12 12:24:28 -0700

    interval

--- date.js
+++ date.js
@@ -1,4 +1,10 @@
 var dateWithoutSecond = new Date();
+
+//update the time every minute
+setInterval(function() {
+  dateWithoutSecond = new Date();
+}, 60000);
+
 dateDiv = document.querySelector(".date");
 dateDiv.innerHTML = dateWithoutSecond.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
 
